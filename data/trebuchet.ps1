840977f9cf2ae5f8af8248bd2e973d87302771fd
justin-sysadmin <justingn@gmail.com>
2023-12-15 21:01:05 -0500

    - Finished Day 1, Parts 1 and 2 (trebuchet.ps1) - Created folder structure - Uploaded Input files for Days 1 (trebuchet.txt) and 2 (cubes.txt)

---
+++ data/trebuchet.ps1
@@ -0,0 +1,64 @@
+###############################################################################
+# Advent of Code 2023: Trebuchet
+# Version: 0.2
+# Author: Infrasaurus
+# Last Modified: 15-DEC-2023
+###############################################################################
+# Import the problem text
+$input = Get-Content ".\problems\trebuchet.txt"
+# Initialize the $solution variable
+$solution = 0
+# Create function to solve the problem, line by line
+function linesolve {
+    # Defines the line input as a string
+    param (
+        [string]$string
+    )
+    # Regex matches for all digits in the line
+    $numbers = [regex]::Matches($string, '(\d)')
+    # For all lines with integer count > 0, grabs first and last digit
+    if ($numbers.Count -gt 0) {
+        $first = $numbers[0].Value.ToLower()
+        Write-Host $first
+        $last = $numbers[$numbers.Count - 1].Value.ToLower()
+        Write-Host $last
+        # Maps words to numbers if needed, using regex
+        if ( $first -notmatch '\d' ) {
+            $first = $map[$first]
+        }
+        if ( $last -notmatch '\d' ) {
+            $last = $map[$last]
+        }
+        # Combines $first and $last into single integer
+        [int]$combined = [int]("$first$last")
+        Write-Output $combined
+    }
+}
+# Create function to combine line solutions into a puzzle solution
+function puzzlesolve {
+    param (
+        [int]$sum,
+        [int]$currentsum
+    )
+    $currentsum += $sum
+    Write-Output $currentsum
+}
+# Parse the problem text line by line, then add each line solution to ongoing total
+ForEach ($line in $input) {
+    # Sanitize data for simpler regex by replacing strings with integers where appropriate
+    $line = $line `
+        -replace "one","o1e" `
+        -replace "two","t2o"`
+        -replace "three","t3e"`
+        -replace "four","f4r"`
+        -replace "five","f5e"`
+        -replace "six","s6x"`
+        -replace "seven","s7n"`
+        -replace "eight","e8t"`
+        -replace "nine","n9e"
+    $increment = linesolve -string $line
+    Write-Host "Incrementing by $increment..."
+    $solution = puzzlesolve -sum $increment -currentsum $solution
+    Write-Host "Current value: $solution"
+}
+Write-Host "If my calculations are correct, the solution is $solution!"
